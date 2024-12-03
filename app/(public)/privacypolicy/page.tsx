8b292484ffe4e49a553785fcee08b04ad3012831
Nadir Yusifov <nadiryusifov448@gmail.com>
2024-12-04 00:57:51 +0400

    first commit

--- app/(public)/privacypolicy/page.tsx
+++ app/(public)/privacypolicy/page.tsx
@@ -11,6 +11,7 @@ export default function PrivacyPolicy() {
             <PrivacyPolicyCollect />
             <PrivacyPolicyInformation />
             <PrivacyPolicyData />
+            fff
         </>
     )
 }
