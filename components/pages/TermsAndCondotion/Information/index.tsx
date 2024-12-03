9dee95b9363f8278c0bd4bd3b6ce6ba4b3f469f9
Nadir Yusifov <nadiryusifov448@gmail.com>
2024-12-04 01:23:35 +0400

    build

--- components/pages/TermsAndCondotion/Information/index.tsx
+++ components/pages/TermsAndCondotion/Information/index.tsx
@@ -18,7 +18,7 @@ export default function Information() {
                                 <li>→ Integer adipiscing erat eget risus sollicitudin pellentesque et non erat. Maecenas nibh dolor, malesuada et bibendum a, sagittis accumsan ipsum.</li>
                             </ul>
                             <p className="mt-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer adipiscing erat eget risus sollicitudin pellentesque et non erat. Maecenas nibh dolor, malesuada et bibendum a, sagittis accumsan ipsum. Pellentesque ultrices ultrices sapien, nec tincidunt nunc posuere ut. Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                Lorem Ipsum has been the industry`&apos;`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                 <br />
                                 <br />
                                 It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
--- components/pages/_app.tsx
+++ components/pages/_app.tsx
@@ -1,14 +1,14 @@
 import Header from "../layout/Header"
 import Footer from "../layout/Footer"
 import type { AppProps } from "next/app"
-import Layout from "@/app/(public)/layout"
+// import Layout from "@/app/(public)/layout"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Layout>
+      <Header/>
         <Component {...pageProps} />
-      </Layout>
+      <Footer/>
     </>
   )
 }
